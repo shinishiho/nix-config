899b449c392848d8c7aae17f4c09f9f1cb9f2d9e
ShiniShiho <59284549+shinishiho@users.noreply.github.com>
2026-02-19 08:05:23 +0700

    update: fish plugins

--- home/config/fish/default.nix
+++ home/config/fish/default.nix
@@ -6,7 +6,6 @@
 {
   imports = [
     ./plugins.nix
-    ./functions/yazi.nix
   ];
 
   programs.fish = {
--- home/config/fish/plugins.nix
+++ home/config/fish/plugins.nix
@@ -9,8 +9,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "franciscolourenco";
         repo = "done";
-        rev = "0bfe402753681f705a482694fcaf20c2bfc6deb7";
-        sha256 = "sha256-WA6DBrPBuXRIloO05UBunTJ9N01d6tO1K1uqojjO0mo=";
+        rev = "998ad4f5fc9cee36c09840a7e635b56428e554f9";
+        sha256 = "sha256-GZ1ZpcaEfbcex6XvxOFJDJqoD9C5out0W4bkkn768r0=";
       };
     }
     {

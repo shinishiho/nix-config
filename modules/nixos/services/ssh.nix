921519aee7fa0ae3a9c18cd8a01c375d6b138e2d
ShiniShiho <59284549+shinishiho@users.noreply.github.com>
2026-03-28 08:01:36 +0700

    fix: persist ssh machine key

--- modules/nixos/services/ssh.nix
+++ modules/nixos/services/ssh.nix
@@ -29,5 +29,12 @@ in
       enable = true;
       enableSSHSupport = false;
     };
+
+    environment.persistence."/persistent".files = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/etc/ssh/ssh_host_ed25519_key.pub"
+      "/etc/ssh/ssh_host_rsa_key"
+      "/etc/ssh/ssh_host_rsa_key.pub"
+    ];
   };
 }

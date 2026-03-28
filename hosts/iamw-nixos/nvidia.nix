22190b873ff29d0767e9aa67e54e645f86fdf56a
ShiniShiho <59284549+shinishiho@users.noreply.github.com>
2026-03-28 08:02:09 +0700

    fix: nvidia suspend issue

--- hosts/iamw-nixos/nvidia.nix
+++ hosts/iamw-nixos/nvidia.nix
@@ -22,11 +22,11 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true;
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+    powerManagement.finegrained = true;
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
@@ -43,7 +43,7 @@
         enableOffloadCmd = true;
       };
 
-      intelBusId = "PCI:0@0:2:0";
+      amdgpuBusId = "PCI:12@0:0:0";
       nvidiaBusId = "PCI:1@0:0:0";
     };
 

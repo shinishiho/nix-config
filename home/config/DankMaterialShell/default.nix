882e4c6a47a5496192ddae6dde08c806c458cee0
ShiniShiho <59284549+shinishiho@users.noreply.github.com>
2026-07-25 21:38:36 +0700

    fix(dms): `dms-plugin-registry` module

--- home/config/DankMaterialShell/default.nix
+++ home/config/DankMaterialShell/default.nix
@@ -9,7 +9,7 @@
 {
   imports = [
     inputs.dms.homeModules.dank-material-shell
-    inputs.dms-plugin-registry.modules.default
+    inputs.dms-plugin-registry.homeModules.default
     inputs.dms.homeModules.niri
 
     ./plugins
